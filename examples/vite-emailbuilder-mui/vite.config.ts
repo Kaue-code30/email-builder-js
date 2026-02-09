88686c6a338b1d7bf3a5e04047d2a36b801d4fc2
Kaue de Lima <kaue.lima@uxgroup.com.br>
2026-02-09 10:57:52 -0300

    Teste

--- examples/vite-emailbuilder-mui/vite.config.ts
+++ examples/vite-emailbuilder-mui/vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: process.env.VERCEL ? '/' : '/email-builder-js/',
+   build: {
+    outDir: '../../dist',
+    emptyOutDir: true,
+  },
 });
 
